0448429ad673b253bb777cc411883a188eb017a1
hernanruscica <cesarhernanruscica@gmail.com>
2022-04-20 17:47:49 -0300

    mejorado el diseño del header. manejador de clicks y mostrador de estados de clicks en la consola

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,41 @@
 let subcategorias; 
-let imagesUrl = "./imagenes/"      
+let imagesUrl = "./imagenes/"    
+let requerimientosEstado = 0, subcategoriasEstado = 0;  
+const $requerimientoItems = document.getElementsByClassName("requerimiento-item");
+const $subCategoriasItems = document.getElementsByClassName("subcategoria-item");
 const $subcategorias = document.getElementById("subcategorias");
 const $ofertas = document.getElementById("ofertas");
 
-
+const leyendoClicksRequerimientos = () => {
+    //leyendo los clicks en los requerimientos
+    for (let i = 0; i < $requerimientoItems.length; i++){    
+        $requerimientoItems[i].addEventListener("click", function() {   
+            requerimientosEstado = i;
+            mostrarEstados();
+            var current = document.getElementsByClassName("activeReq");        
+            current[0].className = current[0].className.replace(" activeReq", "");
+            this.className += " activeReq";        
+            });
+        }
+}
+const leyendoClicksSubcategorias = () => {
+    //leyendo los clicks en las subcategorias
+    for (let i = 0; i < $subCategoriasItems.length; i++){    
+        $subCategoriasItems[i].addEventListener("click", function() {   
+            subcategoriasEstado = i;
+            mostrarEstados();
+            var current = document.getElementsByClassName("activeSub");        
+            current[0].className = current[0].className.replace(" activeSub", "");
+            this.className += " activeSub";        
+            });
+    }
+}
+ 
+const mostrarEstados = () => {
+    console.clear();
+    console.log("Requerimiento: " + requerimientosEstado +"/n");
+    console.log("Subcategoria: " + subcategoriasEstado +"/n");
+}
 
 const crearNuevoSubcategoriaItem = (titulo, imagen) => {
     //inicio de creacion de la subcategoria        
@@ -65,27 +97,31 @@ const crearNuevoOfertaItem = (titulo, descripcion, icono) => {
 const llenarSubcategorias = (subcats) => {
     subcats.forEach(element => {                
         const $nuevoSubcategoriaItem = crearNuevoSubcategoriaItem(element.titulo, element.icono);
+        if (element.id == 0) { $nuevoSubcategoriaItem.classList.add("activeSub"); }
+
         $subcategorias.appendChild($nuevoSubcategoriaItem);
-    });
-    console.log("llenando subcats");
-}
+        });
+        console.log("llenando subcats");
+    }
 const llenarOfertas = (ofertas) => {
     ofertas.forEach(element => {
         const $nuevoOfertaItem = crearNuevoOfertaItem(element.titulo, element.descripcion, element.icono);
         $ofertas.appendChild($nuevoOfertaItem);
-    })
-}
+        })
+    }
 async function populate(){
-const requestURL = 'https://raw.githubusercontent.com/hernanruscica/ServiciosInformaticosSoporte/master/sistemas-ayuda.json';
-const request = new Request(requestURL);
+    const requestURL = 'https://raw.githubusercontent.com/hernanruscica/ServiciosInformaticosSoporte/master/sistemas-ayuda.json';
+    const request = new Request(requestURL);
 
-const response = await fetch(request);
-const subcategoriasText = await response.text();
+    const response = await fetch(request);
+    const subcategoriasText = await response.text();
 
-subcategorias = JSON.parse(subcategoriasText);            
-}
-populate().then(() => {                  
+    subcategorias = JSON.parse(subcategoriasText);            
+    }
+populate().then(() => {  
+        leyendoClicksRequerimientos();                
         llenarSubcategorias(subcategorias["subcategorias"]);
-        llenarOfertas(subcategorias["subcategorias"]["0"]["ofertas"]);
+        leyendoClicksSubcategorias();
+        llenarOfertas(subcategorias["subcategorias"]["2"]["ofertas"]);        
         });          
-console.clear();
+    
