dc345d5f12dd0892778c16bff3152d3156fbdb2b
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-05-24 17:44:36 -0300

    Add files via upload
    terminado, hasta el copiado del enlace con un boton.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ServiciosInformaticosSoporte
+<h1>pagina que arma un enlace html</h1>
+<p>Ademas agrega el target= '_blank'<br>
+<a href= '' >Demo</a> </p>
---
+++ scripts.js
@@ -0,0 +1,115 @@
+    /* 
+    let enlace = 'https://soportesistemas.trabajo.gob.ar/KnowledgeBase/View/13#/';
+    let titulo = '¿Cómo realizar el forzado de clave para usuarios de red?';
+
+    const $fragment = document.createDocumentFragment();
+    const $pre = document.createElement("pre");
+    const $code = document.createElement("code");
+    const $a = document.createElement("a");    
+
+    const $enlace = document.getElementById('enlace');
+    const $enlaceFormatoHTMLid = document.getElementById('enlaceFormatoHTML');
+
+    $a.setAttribute('href', enlace);
+    $a.setAttribute('target', '_blank');
+    $a.innerHTML = titulo;      
+    
+    $enlace.appendChild($a);    
+
+    const menor = '&lt;';
+    const mayor = '&gt'; 
+    let aTexto = `<a href = '${enlace}' target = '_blank'>${titulo}</a>`;    
+    aTexto = aTexto.replaceAll('<', menor);
+    aTexto = aTexto.replaceAll('>', mayor);
+    
+    $code.innerHTML = aTexto;
+    $pre.appendChild($code);
+    $fragment.appendChild($pre);             
+    
+    $enlaceFormatoHTMLid.appendChild($fragment);
+   */
+
+    let enlace, titulo;
+    //recibe los ids de los inputs
+    function getVariables(urlId, tituloId) {
+        const $url = document.getElementById(urlId);
+        const $titulo = document.getElementById(tituloId);
+        enlace = $url.value;
+        titulo = $titulo.value;
+        //console.log(titulo, enlace);
+        generarEnlace('enlace', enlace, titulo);
+        generarEnlaceHTML('enlaceFormatoHTML', enlace, titulo)
+        
+    }
+    getVariables('inputUrl', 'inputTitulo')
+
+    function generarEnlace(enlaceId, enlace, titulo){
+        const $fragment = document.createDocumentFragment();
+        const $a = document.createElement("a");  
+        const $tituloVistaPrevia = document.createElement('h3');
+        $tituloVistaPrevia.innerHTML = 'Vista Previa del enlace:'  
+
+        const $enlace = document.getElementById(enlaceId);        
+
+        $a.setAttribute('href', enlace);
+        $a.setAttribute('target', '_blank');
+        $a.innerHTML = titulo;     
+        //console.log($a);          
+        $enlace.innerHTML = '';    
+        $enlace.appendChild($tituloVistaPrevia);   
+        $enlace.appendChild($a);    
+    }
+/* 
+<textarea name="" id="" rows="5" disabled = "true" class = "contenedor enlaceFormatoHTML">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio placeat, asperiores fugit praesentium quo ipsa, a porro quibusdam hic sed, vel nisi qui. Tenetur aliquam assumenda natus autem quod odit?
+</textarea> 
+*/
+
+    function generarEnlaceHTML(enlaceHTMLId, enlace, titulo){
+
+        const $fragment = document.createDocumentFragment();
+        const $textArea = document.createElement("textarea");
+        $textArea.setAttribute('rows', 5);
+        $textArea.setAttribute('disabled', true);
+        $textArea.setAttribute('id', 'enlaceFormatoHTMLid');
+        $textArea.classList.add('enlaceFormatoHTML');
+        $textArea.classList.add('contenedor');
+        //const $code = document.createElement("code");
+        const $enlaceFormatoHTMLid = document.getElementById(enlaceHTMLId)
+        const menor = '&lt';
+        const mayor = '&gt'; 
+        let aTexto = `<a href = '${enlace}' target = '_blank'>${titulo}</a>`;    
+        aTexto = aTexto.replaceAll('<', menor);
+        aTexto = aTexto.replaceAll('>', mayor);
+        
+        $textArea.innerHTML = aTexto;
+        //$pre.appendChild($code);
+        $fragment.appendChild($textArea);   
+        //<button id="btn" onclick="copyToClickBoard()">Copy</button>
+        const $btnEnlace = document.createElement('button');
+        $btnEnlace.setAttribute('onclick', 'copyToClickBoard()')
+        $btnEnlace.innerHTML = "Copiar";
+        $fragment.appendChild($btnEnlace);
+        
+        $enlaceFormatoHTMLid.innerHTML = '';
+        $enlaceFormatoHTMLid.appendChild($fragment);
+        
+    }
+    
+    function copyToClickBoard(){
+        let content = document.getElementById('enlaceFormatoHTMLid').innerHTML;
+        const menor = '&lt;';
+        const mayor = '&gt;'; 
+        content = content.replaceAll('&lt;', '<');
+        content = content.replaceAll('&gt;', '>');
+    
+        navigator.clipboard.writeText(content)
+            .then(() => {
+            console.log("Text copied to clipboard...")
+        })
+            .catch(err => {
+            console.log('Something went wrong', err);
+        })
+     
+    }   
+
