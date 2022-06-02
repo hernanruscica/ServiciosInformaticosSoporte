c177921dff521ac335a95748ff3121111e91167c
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-06-02 13:02:50 -0300

    Add files via upload
    agregado un titulo y la descripción de la funcionalidad. reorganice el código un poco.

--- armadoEnlacesHTML/scripts.js
+++ armadoEnlacesHTML/scripts.js
@@ -1,126 +1,88 @@
-    /* 
-    let enlace = 'https://soportesistemas.trabajo.gob.ar/KnowledgeBase/View/13#/';
-    let titulo = '¿Cómo realizar el forzado de clave para usuarios de red?';
+let enlace, titulo;
+//recibe los ids de los inputs
+function getVariables(urlId, tituloId) {
+    const $url = document.getElementById(urlId);
+    const $titulo = document.getElementById(tituloId);
+    enlace = $url.value;
+    titulo = $titulo.value;
+    //console.log(titulo, enlace);
+    generarEnlace('enlace', enlace, titulo);
+    generarEnlaceHTML('enlaceFormatoHTML', enlace, titulo)        
+}
+getVariables('inputUrl', 'inputTitulo')
 
+function generarEnlace(enlaceId, enlace, titulo){
     const $fragment = document.createDocumentFragment();
-    const $pre = document.createElement("pre");
-    const $code = document.createElement("code");
-    const $a = document.createElement("a");    
+    const $a = document.createElement("a");  
+    const $tituloVistaPrevia = document.createElement('h3');
+    $tituloVistaPrevia.innerHTML = 'Vista Previa del enlace:'  
+    $tituloVistaPrevia.classList.add('titulos_sueltos')
 
-    const $enlace = document.getElementById('enlace');
-    const $enlaceFormatoHTMLid = document.getElementById('enlaceFormatoHTML');
+    const $enlace = document.getElementById(enlaceId);        
 
     $a.setAttribute('href', enlace);
     $a.setAttribute('target', '_blank');
-    $a.innerHTML = titulo;      
-    
+    $a.innerHTML = titulo;     
+    //console.log($a);          
+    $enlace.innerHTML = '';    
+    $enlace.appendChild($tituloVistaPrevia);   
     $enlace.appendChild($a);    
+}
 
-    const menor = '&lt;';
+function generarEnlaceHTML(enlaceHTMLId, enlace, titulo){
+
+    const $fragment = document.createDocumentFragment();
+    const $textArea = document.createElement("textarea");
+    $textArea.setAttribute('rows', 5);
+    $textArea.setAttribute('disabled', true);
+    $textArea.setAttribute('id', 'enlaceFormatoHTMLid');
+    $textArea.classList.add('enlaceFormatoHTML');
+    $textArea.classList.add('contenedor');
+    //const $code = document.createElement("code");
+    const $enlaceFormatoHTMLid = document.getElementById(enlaceHTMLId)
+    const menor = '&lt';
     const mayor = '&gt'; 
     let aTexto = `<a href = '${enlace}' target = '_blank'>${titulo}</a>`;    
     aTexto = aTexto.replaceAll('<', menor);
     aTexto = aTexto.replaceAll('>', mayor);
     
-    $code.innerHTML = aTexto;
-    $pre.appendChild($code);
-    $fragment.appendChild($pre);             
+    $textArea.innerHTML = aTexto;
+    //$pre.appendChild($code);
+    $fragment.appendChild($textArea);   
+    //<button id="btn" onclick="copyToClickBoard()">Copy</button>
+    const $btnEnlace = document.createElement('button');
+    $btnEnlace.setAttribute('onclick', 'copyToClickBoard()')
+    $btnEnlace.innerHTML = "COPIAR";
+    $btnEnlace.classList.add('btn');
+    $fragment.appendChild($btnEnlace);
+
+    const $labelCopiado = document.createElement('label');
+    $labelCopiado.classList.add('label_copiado');        
+    $labelCopiado.classList.add('oculto');
+    $labelCopiado.innerHTML = "COPIADO ...";
+    $fragment.appendChild($labelCopiado);
     
-    $enlaceFormatoHTMLid.appendChild($fragment);
-   */
-
-    let enlace, titulo;
-    //recibe los ids de los inputs
-    function getVariables(urlId, tituloId) {
-        const $url = document.getElementById(urlId);
-        const $titulo = document.getElementById(tituloId);
-        enlace = $url.value;
-        titulo = $titulo.value;
-        //console.log(titulo, enlace);
-        generarEnlace('enlace', enlace, titulo);
-        generarEnlaceHTML('enlaceFormatoHTML', enlace, titulo)
-        
-    }
-    getVariables('inputUrl', 'inputTitulo')
-
-    function generarEnlace(enlaceId, enlace, titulo){
-        const $fragment = document.createDocumentFragment();
-        const $a = document.createElement("a");  
-        const $tituloVistaPrevia = document.createElement('h3');
-        $tituloVistaPrevia.innerHTML = 'Vista Previa del enlace:'  
-        $tituloVistaPrevia.classList.add('titulos_sueltos')
-
-        const $enlace = document.getElementById(enlaceId);        
-
-        $a.setAttribute('href', enlace);
-        $a.setAttribute('target', '_blank');
-        $a.innerHTML = titulo;     
-        //console.log($a);          
-        $enlace.innerHTML = '';    
-        $enlace.appendChild($tituloVistaPrevia);   
-        $enlace.appendChild($a);    
-    }
-/* 
-<textarea name="" id="" rows="5" disabled = "true" class = "contenedor enlaceFormatoHTML">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio placeat, asperiores fugit praesentium quo ipsa, a porro quibusdam hic sed, vel nisi qui. Tenetur aliquam assumenda natus autem quod odit?
-</textarea> 
-*/
-
-    function generarEnlaceHTML(enlaceHTMLId, enlace, titulo){
-
-        const $fragment = document.createDocumentFragment();
-        const $textArea = document.createElement("textarea");
-        $textArea.setAttribute('rows', 5);
-        $textArea.setAttribute('disabled', true);
-        $textArea.setAttribute('id', 'enlaceFormatoHTMLid');
-        $textArea.classList.add('enlaceFormatoHTML');
-        $textArea.classList.add('contenedor');
-        //const $code = document.createElement("code");
-        const $enlaceFormatoHTMLid = document.getElementById(enlaceHTMLId)
-        const menor = '&lt';
-        const mayor = '&gt'; 
-        let aTexto = `<a href = '${enlace}' target = '_blank'>${titulo}</a>`;    
-        aTexto = aTexto.replaceAll('<', menor);
-        aTexto = aTexto.replaceAll('>', mayor);
-        
-        $textArea.innerHTML = aTexto;
-        //$pre.appendChild($code);
-        $fragment.appendChild($textArea);   
-        //<button id="btn" onclick="copyToClickBoard()">Copy</button>
-        const $btnEnlace = document.createElement('button');
-        $btnEnlace.setAttribute('onclick', 'copyToClickBoard()')
-        $btnEnlace.innerHTML = "COPIAR";
-        $btnEnlace.classList.add('btn');
-        $fragment.appendChild($btnEnlace);
-
-        const $labelCopiado = document.createElement('label');
-        $labelCopiado.classList.add('label_copiado');        
-        $labelCopiado.classList.add('oculto');
-        $labelCopiado.innerHTML = "COPIADO ...";
-        $fragment.appendChild($labelCopiado);
-        
-        $enlaceFormatoHTMLid.innerHTML = '';
-        $enlaceFormatoHTMLid.appendChild($fragment);
-        
-    }
+    $enlaceFormatoHTMLid.innerHTML = '';
+    $enlaceFormatoHTMLid.appendChild($fragment);        
+}
     
-    function copyToClickBoard(){
-        let content = document.getElementById('enlaceFormatoHTMLid').innerHTML;
-        const menor = '&lt;';
-        const mayor = '&gt;'; 
-        content = content.replaceAll('&lt;', '<');
-        content = content.replaceAll('&gt;', '>');
-    
-        navigator.clipboard.writeText(content)
-            .then(() => {
-            console.log("Text copied to clipboard...")
-        })
-            .catch(err => {
-            console.log('Something went wrong', err);
-        })
-        mostrarMensaje('label_copiado');
-        setTimeout(() => ocultarMensaje('label_copiado'), 2000);
-    }   
+function copyToClickBoard(){
+    let content = document.getElementById('enlaceFormatoHTMLid').innerHTML;
+    const menor = '&lt;';
+    const mayor = '&gt;'; 
+    content = content.replaceAll('&lt;', '<');
+    content = content.replaceAll('&gt;', '>');
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        console.log("Text copied to clipboard...")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+    mostrarMensaje('label_copiado');
+    setTimeout(() => ocultarMensaje('label_copiado'), 2000);
+}   
 
  function seleccionarTodoTexto(inputId){
     $input = document.getElementById(inputId);    
